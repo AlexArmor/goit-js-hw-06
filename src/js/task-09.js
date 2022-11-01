f6a1c19ae0d4d574ed337ba11590b79109ac9da3
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-01 23:27:32 +0200

    added 9th task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyVariable: document.querySelector('body'),
+  btnColor: document.querySelector('.change-color'),
+  colorInfo: document.querySelector('.color'),
+}
+
+refs.btnColor.addEventListener('click', () => {
+  refs.bodyVariable.style.backgroundColor = getRandomHexColor();
+  refs.colorInfo.textContent = refs.bodyVariable.style.backgroundColor;
+});
