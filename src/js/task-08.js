f8546b97fabc15889a2ab0fa8549fc3bd30f196c
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-01 23:01:20 +0200

    added 8th task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (email === "" || password === "") {
+        alert("Error! Please, fill all fields");
+    } else {
+        const formData = {
+            email,
+            password,
+        }
+
+        console.log(formData);
+        form.reset();
+    }
+}
