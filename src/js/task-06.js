447d9463684eab0c2e51276025ddb74e14efddae
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-01 21:12:55 +0200

    added 6th task

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const inputField = document.querySelector("#validation-input");
+
+const onInputBlur = event => {
+    const inputLength = event.currentTarget.value.length;
+    const dataAttributeLength = Number(inputField.dataset.length);
+
+    if (inputLength === dataAttributeLength) {
+        inputField.classList.remove('invalid');
+        inputField.classList.add('valid');
+    } else {
+        inputField.classList.add('invalid');
+    }
+}
+
+inputField.addEventListener('blur', onInputBlur);
